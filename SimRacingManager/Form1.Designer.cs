c5bb2c283c96880287bea8048ec511e7fea295f6
msastre23 <mikisastre23@gmail.com>
2024-05-14 19:47:05 +0200

    firsteps

--- SimRacingManager/Form1.Designer.cs
+++ SimRacingManager/Form1.Designer.cs
@@ -28,20 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newCompetitionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newRecordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importRecordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportRecordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabHome = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1.SuspendLayout();
+            this.tabHome.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(832, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newCompetitionToolStripMenuItem,
+            this.newRecordToolStripMenuItem,
+            this.importRecordToolStripMenuItem,
+            this.exportRecordToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 24);
+            this.fileToolStripMenuItem.Text = " File";
+            // 
+            // newCompetitionToolStripMenuItem
+            // 
+            this.newCompetitionToolStripMenuItem.Name = "newCompetitionToolStripMenuItem";
+            this.newCompetitionToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newCompetitionToolStripMenuItem.Text = "New competition";
+            // 
+            // newRecordToolStripMenuItem
+            // 
+            this.newRecordToolStripMenuItem.Name = "newRecordToolStripMenuItem";
+            this.newRecordToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newRecordToolStripMenuItem.Text = "New record";
+            // 
+            // importRecordToolStripMenuItem
+            // 
+            this.importRecordToolStripMenuItem.Name = "importRecordToolStripMenuItem";
+            this.importRecordToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.importRecordToolStripMenuItem.Text = "Import record";
+            // 
+            // exportRecordToolStripMenuItem
+            // 
+            this.exportRecordToolStripMenuItem.Name = "exportRecordToolStripMenuItem";
+            this.exportRecordToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exportRecordToolStripMenuItem.Text = "Export record";
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(76, 24);
+            this.settingToolStripMenuItem.Text = "Settings";
+            this.settingToolStripMenuItem.Click += new System.EventHandler(this.settingToolStripMenuItem_Click);
+            // 
+            // tabHome
+            // 
+            this.tabHome.Controls.Add(this.tabPage1);
+            this.tabHome.Controls.Add(this.tabPage2);
+            this.tabHome.Location = new System.Drawing.Point(12, 31);
+            this.tabHome.Name = "tabHome";
+            this.tabHome.SelectedIndex = 0;
+            this.tabHome.Size = new System.Drawing.Size(808, 418);
+            this.tabHome.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(800, 389);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "HOME";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(800, 389);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "RESULTS";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(832, 461);
+            this.Controls.Add(this.tabHome);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabHome.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newCompetitionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newRecordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importRecordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportRecordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabHome;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
--- SimRacingManager/Form1.cs
+++ SimRacingManager/Form1.cs
@@ -16,5 +16,10 @@ namespace SimRacingManager
         {
             InitializeComponent();
         }
+
+        private void settingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
